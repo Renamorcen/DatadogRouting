cd1126b20193b4c256a47ccb1061d4781deeee0e
Renamorcen <renamorcen@gmail.com>
2019-02-27 19:05:21 +0000

    Suparsinau visa info

--- DatadogRouting.go
+++ DatadogRouting.go
@@ -17,15 +17,16 @@ import	(
 	"strconv"
 )
 
-//linkedlist 
+//Struktura laikyti viena lokacijos entry
 type geocode struct{
-	id	int
-	brewery_id int
-	lat float32
-	lon float32
-	accuracy string
+	id		int
+	brewery_id	int
+	lat		float64
+	lon		float64
+	accuracy	string
 }
 
+//Dummy code algoritmui
 
 func greedyAlg(long, lat int) int{
 	return 0
@@ -38,13 +39,22 @@ func main(){
 	reader := csv.NewReader(bytes.NewReader(B_geocodes))
 	geocodes, err := reader.ReadAll()
 	fmt.Println(lat+"/"+lon)
-	fmt.Printf("%s",geocodes[2][1])
 	geocodeSlice :=  make([]geocode, len(geocodes))
 	for i:=range geocodes{
-		id := strconv
-		geocodeC:=geocode{strconv.Atoi(geocodes[i][0]), geocodes[i][1], geocodes[i][2], geocodes[i][3], geocodes[i][4]}
-		//fmt.Println(geocodes[i][4])
+		if i!=0{
+			id, err		:= strconv.Atoi(geocodes[i][0])
+			brewery_id, err := strconv.Atoi(geocodes[i][1])
+			lat, err	:= strconv.ParseFloat(geocodes[i][2], 32)
+			lon, err	:= strconv.ParseFloat(geocodes[i][3], 32)
+			accuracy	:= geocodes[i][4]
+			if err!=nil{
+				fmt.Println(err)
+			}
+			geocodeC:=geocode{id, brewery_id, lat, lon, accuracy}
+			geocodeSlice[i-1] = geocodeC
+		}
 	}
+	fmt.Println(geocodeSlice[1].lat)
 	if err!=nil{
 		fmt.Println(err)
 	}
