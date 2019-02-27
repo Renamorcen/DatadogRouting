11423c13bed35914bbc190be8fcfc0c4ca142352
Renamorcen <renamorcen@gmail.com>
2019-02-27 23:24:40 +0000

    Added in the beer database

--- DatadogRouting.go
+++ DatadogRouting.go
@@ -9,7 +9,7 @@
 package main
 
 import	(
-	//"os"
+	"os"
 	"fmt"
 	"io/ioutil"
 	"encoding/csv"
@@ -39,34 +39,63 @@ func greedyAlg(long, lat int) int{
 	return 0
 }
 
-func parseGeocodes() []geocode {
-	B_geocodes, err := ioutil.ReadFile("dumps/geocodes.csv")
-	reader := csv.NewReader(bytes.NewReader(B_geocodes))
-	geocodes, err := reader.ReadAll()
-	var geocodeSlice []geocode
-	for i:=range geocodes{
+func getStrings(location string) [][]string{
+	B_content, err := ioutil.ReadFile(location)
+	reader := csv.NewReader(bytes.NewReader(B_content))
+	strings, err := reader.ReadAll()
+	if err!=nil{
+		fmt.Println(err)
+	}
+	return strings
+}
+
+func parseBeers() []beer {
+	S_beer := getStrings("dumps/beers.csv")
+	var beerSlice []beer
+	for i:=range S_beer{
 		if i!=0{
-			id, err		:= strconv.Atoi(geocodes[i][0])
-			brewery_id, err := strconv.Atoi(geocodes[i][1])
-			lat, err	:= strconv.ParseFloat(geocodes[i][2], 32)
-			lon, err	:= strconv.ParseFloat(geocodes[i][3], 32)
-			accuracy	:= geocodes[i][4]
+			id, err		:= strconv.Atoi(S_beer[i][0])
+			brewery_id, err := strconv.Atoi(S_beer[i][1])
+			name		:= S_beer[i][2]
+			cat_id, err	:= strconv.Atoi(S_beer[i][3])
+			style_id, err	:= strconv.Atoi(S_beer[i][4])
 			if err!=nil{
 				fmt.Println(err)
 			}
-			geocodeC:=geocode{id, brewery_id, lat, lon, accuracy}
-			geocodeSlice = append(geocodeSlice,geocodeC)
+			beerObj:=beer{id, brewery_id, name, cat_id, style_id}
+			beerSlice = append(beerSlice,beerObj)
 		}
 	}
-	if err!=nil{
-		fmt.Println(err)
+	return beerSlice
+}
+
+func parseGeocodes() []geocode {
+	S_geocodes := getStrings("dumps/geocodes.csv")
+	var geocodeSlice []geocode
+	for i:=range S_geocodes{
+		if i!=0{
+			id, err		:= strconv.Atoi(S_geocodes[i][0])
+			brewery_id, err := strconv.Atoi(S_geocodes[i][1])
+			lat, err	:= strconv.ParseFloat(S_geocodes[i][2], 32)
+			lon, err	:= strconv.ParseFloat(S_geocodes[i][3], 32)
+			accuracy	:= S_geocodes[i][4]
+			if err!=nil{
+				fmt.Println(err)
+			}
+			geocodeObj:=geocode{id, brewery_id, lat, lon, accuracy}
+			geocodeSlice = append(geocodeSlice,geocodeObj)
+		}
 	}
 	return geocodeSlice
 }
 
 func main(){
-	//lon := os.Args[1]
-	//lat := os.Args[2]
+	lon := os.Args[1]
+	lat := os.Args[2]
+	fmt.Println(lon + "/" + lat)
+	//parsinu beers.csv
+	beerSlice := parseBeers()
+	fmt.Println(beerSlice[1].name)
 	//Parsinu geocodes.csv
 	geocodeSlice := parseGeocodes()
 	fmt.Println(geocodeSlice[1].lat)
